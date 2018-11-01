19ad0826948bf9228e1b87fd45508ebd4b84739f
Michael McMahon <mjmcmahon@outlook.ie>
2018-11-01 23:45:03 +0000

    Second Commit

--- spring-boot-rest/src/main/java/com/example/demo/beans/Student.java
+++ spring-boot-rest/src/main/java/com/example/demo/beans/Student.java
@@ -4,22 +4,28 @@ public class Student {
     String name;
     int age;
     String registrationNumber;
-	public String getName() {
+	
+    public String getName() {
 		return name;
 	}
-	public void setName(String name) {
+	
+    public void setName(String name) {
 		this.name = name;
 	}
-	public int getAge() {
+	
+    public int getAge() {
 		return age;
 	}
-	public void setAge(int age) {
+	
+    public void setAge(int age) {
 		this.age = age;
 	}
-	public String getRegistrationNumber() {
+	
+    public String getRegistrationNumber() {
 		return registrationNumber;
 	}
-	public void setRegistrationNumber(String registrationNumber) {
+	
+    public void setRegistrationNumber(String registrationNumber) {
 		this.registrationNumber = registrationNumber;
 	}
 }
--- spring-boot-rest/src/main/java/com/example/demo/controllers/StudentUpdateController.java
+++ spring-boot-rest/src/main/java/com/example/demo/controllers/StudentUpdateController.java
@@ -1,24 +1,22 @@
 package com.example.demo.controllers;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 
 import com.example.demo.beans.Student;
 import com.example.demo.beans.StudentRegistration;
 
 @Controller
-public class StudentRetrieveController {
+public class StudentUpdateController {
 	
-	 @RequestMapping(method = RequestMethod.GET, value="/student/allstudent")
-	  
-	 @ResponseBody
-	  public List<Student> getAllStudents() {
-	  return StudentRegistration.getInstance().getStudentRecords();
-	  }
-
+	@RequestMapping(method = RequestMethod.PUT, value="/update/student")
+	
+	@ResponseBody
+	public String updateStudentRecord(@RequestBody Student stdn) {
+	System.out.println("In updateStudentRecord");   
+	    return StudentRegistration.getInstance().upDateStudent(stdn);
+	}
 }
